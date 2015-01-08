cd9201e0f57cf4d29bacfd9f13011f9594eddbfe
Roni Saha <roni.cse@gmail.com>
2015-01-08 18:45:24 +0600

    Core refactored

--- Tools/Converter.php
+++ Tools/Converter.php
@@ -18,7 +18,8 @@ class Converter
      * @param int $morning
      * @return array
      */
-    public static function getBengaliDateMonthYear(\DateTime $time, $morning = 6) {
+    public static function getBengaliDateMonthYear(\DateTime $time, $morning = 6)
+    {
 
         $enMonth = $time->format('n');
         $day = (int)$time->format('j');
@@ -27,9 +28,9 @@ class Converter
         list($bnDate, $bnMonth) = self::getBengaliDateAndMonth($enMonth, $day, $hour, $morning, $time->format('L'));
 
         return array(
-            'date'   => $bnDate,
-            'month'  => $bnMonth,
-            'year'   => self::getBengaliYear($enMonth, $day, $hour, $morning, $time)
+            'date'  => $bnDate,
+            'month' => $bnMonth,
+            'year'  => self::getBengaliYear($enMonth, $day, $hour, $morning, $time)
         );
     }
 
@@ -45,21 +46,190 @@ class Converter
     private static function getBengaliDateAndMonth($month, $day, $hour, $morning, $isLeapYear)
     {
         switch ($month) {
-            case 1: return self::convertDatesOfJanuary($day, $hour, $morning);
+            case 1: return self::convertDatesOfJanuaryOrApril($day, $hour, $morning, 9);
             case 2: return self::convertDatesOfFebruary($day, $hour, $morning);
             case 3: return self::convertDatesOfMarch($day, $hour, $morning, $isLeapYear);
-            case 4: return self::convertDatesOfApril($day, $hour, $morning);
-            case 5: return self::convertDatesOfMay($day, $hour, $morning);
-            case 6: return self::convertDatesOfJune($day, $hour, $morning);
-            case 7: return self::convertDatesOfJuly($day, $hour, $morning);
-            case 8: return self::convertDatesOfAugust($day, $hour, $morning);
-            case 9: return self::convertDatesOfSeptember($day, $hour, $morning);
+            case 4: return self::convertDatesOfJanuaryOrApril($day, $hour, $morning, 12);
+            case 5: return self::convertDatesOfMayOrJune($day, $hour, $morning, 1);
+            case 6: return self::convertDatesOfMayOrJune($day, $hour, $morning, 2);
+            case 7: return self::convertDatesOfJulyOrAugustOrSeptember($day, $hour, $morning, 3);
+            case 8: return self::convertDatesOfJulyOrAugustOrSeptember($day, $hour, $morning, 4);
+            case 9: return self::convertDatesOfJulyOrAugustOrSeptember($day, $hour, $morning, 5);
             case 10: return self::convertDatesOfOctober($day, $hour, $morning);
-            case 11: return self::convertDatesOfNovember($day, $hour, $morning); break;
-            default : return self::convertDatesOfDecember($day, $hour, $morning);
+            case 11: return self::convertDatesOfNovemberOrDecember($day, $hour, $morning, 7);
+            default : return self::convertDatesOfNovemberOrDecember($day, $hour, $morning, 8);
         }
     }
 
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $bnMonth
+     * @return array
+     */
+    private static function convertDatesOfJanuaryOrApril($day, $hour, $morning, $bnMonth)
+    {
+        if ($day >= 1 && $day <= 13) {
+            return array(self::getNextDayIfNot($day + 16, $hour < $morning), $bnMonth);
+        }
+
+        if ($day == 14 && $hour < $morning) {
+            return array($day + 16, $bnMonth);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $bnMonth, 14);
+    }
+
+    /**
+     * @param $day
+     * @param $stillYesterday
+     * @return mixed
+     */
+    private static function getNextDayIfNot($day, $stillYesterday)
+    {
+        return $stillYesterday ? $day : $day + 1;
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $month
+     * @param $offset
+     * @return array
+     */
+    private static function getDateForNextMonth($day, $hour, $morning, $month, $offset)
+    {
+        return array(self::getNextDayIfNot($day - $offset, $hour < $morning), self::getNextMonth($month));
+    }
+
+    /**
+     * @param $month
+     * @return mixed
+     */
+    private static function getNextMonth($month)
+    {
+        return $month == 12 ? 1 : $month + 1;
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @return array
+     */
+    private static function convertDatesOfFebruary($day, $hour, $morning)
+    {
+        $month = 10;
+
+        if ($day >= 1 && $day <= 12) {
+            return array(self::getNextDayIfNot($day + 17, $hour < $morning), $month);
+        }
+
+        if ($day == 13 && $hour < $morning) {
+            return array($day + 17, $month);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $month, 13);
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $isLeapYear
+     * @return array
+     */
+    private static function convertDatesOfMarch($day, $hour, $morning, $isLeapYear)
+    {
+        $month = 11;
+
+        if ($day >= 1 && $day <= 14) {
+            return array(self::getNextDayIfNot($day + self::getNextDayIfNot(15, $hour < $morning), !$isLeapYear), $month);
+        }
+
+        if ($day == 15 && $hour < $morning) {
+            return array(self::getNextDayIfNot($day + 15, !$isLeapYear), $month);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $month, 15);
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $bnMonth
+     * @return array
+     */
+    private static function convertDatesOfMayOrJune($day, $hour, $morning, $bnMonth)
+    {
+        if ($day >= 1 && $day <= 14) {
+            return array(self::getNextDayIfNot($day + 16, $hour < $morning), $bnMonth);
+        }
+
+        if ($day == 15 && $hour < $morning) {
+            return array($day + 16, $bnMonth);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $bnMonth, 15);
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $bnMonth
+     * @return array
+     */
+    private static function convertDatesOfJulyOrAugustOrSeptember($day, $hour, $morning, $bnMonth)
+    {
+        if ($day >= 1 && $day <= 15) {
+            return array(self::getNextDayIfNot($day + 15, $hour < $morning), $bnMonth);
+        }
+
+        if ($day == 16 && $hour < $morning) {
+            return array($day + 15, $bnMonth);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $bnMonth, 16);
+    }
+
+    private static function convertDatesOfOctober($day, $hour, $morning)
+    {
+        $month = 6;
+
+        if ($day >= 1 && $day <= 15) {
+            return array(self::getNextDayIfNot($day + 14, $hour < $morning), $month);
+        }
+
+        if ($day == 16 && $hour < $morning) {
+            return array($day + 14, $month);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $month, 16);
+    }
+
+    /**
+     * @param $day
+     * @param $hour
+     * @param $morning
+     * @param $bnMonth
+     * @return array
+     */
+    private static function convertDatesOfNovemberOrDecember($day, $hour, $morning, $bnMonth)
+    {
+        if ($day >= 1 && $day <= 14) {
+            return array(self::getNextDayIfNot($day + 15, $hour < $morning), $bnMonth);
+        }
+
+        if ($day == 15 && $hour < $morning) {
+            return array($day + 15, $bnMonth);
+        }
+
+        return self::getDateForNextMonth($day, $hour, $morning, $bnMonth, 15);
+    }
 
     /**
      * @param $enMonth
@@ -73,361 +243,22 @@ class Converter
     {
         $engYear = (int)$time->format('Y');
 
-        if ($enMonth < 4) {
+        if (self::isBeforeNewYear($enMonth, $day, $hour, $morning)) {
             return $engYear - 594;
-        } else {
-            if ($enMonth == 4) {
-                if (($day < 14) || ($day == 14 && $hour < $morning))
-                    return $engYear - 594;
-                else
-                    return $engYear - 593;
-            }
-
-            return $engYear - 593;
         }
+
+        return $engYear - 593;
     }
 
     /**
+     * @param $enMonth
      * @param $day
      * @param $hour
      * @param $morning
-     * @return array
+     * @return bool
      */
-    private static function convertDatesOfJanuary($day, $hour, $morning)
+    private static function isBeforeNewYear($enMonth, $day, $hour, $morning)
     {
-        if ($day >= 1 && $day <= 13) {
-            if ($hour < $morning) {
-                $date = $day + 16;
-            } else {
-                $date = $day + 17;
-            }
-            $month = 9;
-        } else {
-            if ($day == 14 && $hour < $morning) {
-                $date = $day + 16;
-                $month = 9;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 14;
-                } else {
-                    $date = $day - 13;
-                }
-                $month = 10;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    /**
-     * @param $day
-     * @param $hour
-     * @param $morning
-     * @return array
-     */
-    private static function convertDatesOfFebruary($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 12) {
-            if ($hour < $morning) {
-                $date = $day + 17;
-            } else {
-                $date = $day + 18;
-            }
-            $month = 10;
-        } else {
-            if ($day == 13 && $hour < $morning) {
-                $date = $day + 17;
-                $month = 10;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 13;
-                } else {
-                    $date = $day - 12;
-                }
-                $month = 11;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    /**
-     * @param $day
-     * @param $hour
-     * @param $morning
-     * @param $isLeapYear
-     * @return array
-     */
-    private static function convertDatesOfMarch($day, $hour, $morning, $isLeapYear)
-    {
-        if ($day >= 1 && $day <= 14) {
-            if ($hour < $morning) {
-                if (!$isLeapYear) {
-                    $date = $day + 15;
-                } else {
-                    $date = $day + 16;
-                }
-                $month = 11;
-            } else {
-                if (!$isLeapYear) {
-                    $date = $day + 16;
-                } else {
-                    $date = $day + 17;
-                }
-                $month = 11;
-            }
-        } else {
-            if ($day == 15 && $hour < $morning) {
-                if (!$isLeapYear) {
-                    $date = $day + 15;
-                } else {
-                    $date = $day + 16;
-                }
-                $month = 11;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 15;
-                } else {
-                    $date = $day - 14;
-                }
-                $month = 12;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfApril($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 13) {
-            if ($hour < $morning) {
-                $date = $day + 16;
-            } else {
-                $date = $day + 17;
-            }
-            $month = 12;
-        } else {
-            if ($day == 14 && $hour < $morning) {
-                $date = $day + 16;
-                $month = 12;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 14;
-                } else {
-                    $date = $day - 13;
-                }
-                $month = 1;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfMay($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 14) {
-            if ($hour < $morning) {
-                $date = $day + 16;
-            } else {
-                $date = $day + 17;
-            }
-            $month = 1;
-        } else {
-            if ($day == 15 && $hour < $morning) {
-                $date = $day + 16;
-                $month = 1;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 15;
-                } else {
-                    $date = $day - 14;
-                }
-                $month = 2;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfJune($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 14) {
-            if ($hour < $morning) {
-                $date = $day + 16;
-            } else {
-                $date = $day + 17;
-            }
-            $month = 2;
-        } else {
-            if ($day == 15 && $hour < $morning) {
-                $date = $day + 16;
-                $month = 2;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 15;
-                } else {
-                    $date = $day - 14;
-                }
-                $month = 3;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfJuly($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 15) {
-            if ($hour < $morning) {
-                $date = $day + 15;
-            } else {
-                $date = $day + 16;
-            }
-            $month = 3;
-        } else {
-            if ($day == 16 && $hour < $morning) {
-                $date = $day + 15;
-                $month = 3;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 16;
-                } else {
-                    $date = $day - 15;
-                }
-                $month = 4;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfAugust($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 15) {
-            if ($hour < $morning) {
-                $date = $day + 15;
-            } else {
-                $date = $day + 16;
-            }
-            $month = 4;
-        } else {
-            if ($day == 16 && $hour < $morning) {
-                $date = $day + 15;
-                $month = 4;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 16;
-                } else {
-                    $date = $day - 15;
-                }
-                $month = 5;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfSeptember($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 15) {
-            if ($hour < $morning) {
-                $date = $day + 15;
-            } else {
-                $date = $day + 16;
-            }
-            $month = 5;
-        } else {
-            if ($day == 16 && $hour < $morning) {
-                $date = $day + 15;
-                $month = 5;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 16;
-                } else {
-                    $date = $day - 15;
-                }
-                $month = 6;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfOctober($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 15) {
-            if ($hour < $morning) {
-                $date = $day + 14;
-            } else {
-                $date = $day + 15;
-            }
-            $month = 6;
-        } else {
-            if ($day == 16 && $hour < $morning) {
-                $date = $day + 14;
-                $month = 6;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 16;
-                } else {
-                    $date = $day - 15;
-                }
-                $month = 7;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfNovember($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 14) {
-            if ($hour < $morning) {
-                $date = $day + 15;
-            } else {
-                $date = $day + 16;
-            }
-            $month = 7;
-        } else {
-            if ($day == 15 && $hour < $morning) {
-                $date = $day + 15;
-                $month = 7;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 15;
-                } else {
-                    $date = $day - 14;
-                }
-                $month = 8;
-            }
-        }
-
-        return array($date, $month);
-    }
-
-    private static function convertDatesOfDecember($day, $hour, $morning)
-    {
-        if ($day >= 1 && $day <= 14) {
-            if ($hour < $morning) {
-                $date = $day + 15;
-            } else {
-                $date = $day + 16;
-            }
-            $month = 8;
-        } else {
-            if ($day == 15 && $hour < $morning) {
-                $date = $day + 15;
-                $month = 8;
-            } else {
-                if ($hour < $morning) {
-                    $date = $day - 15;
-                } else {
-                    $date = $day - 14;
-                }
-                $month = 9;
-            }
-        }
-
-        return array($date, $month);
+        return $enMonth < 4 || ($enMonth == 4 && (($day < 14) || ($day == 14 && $hour < $morning)));
     }
 }
